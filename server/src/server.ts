b477c4072d80fb3ce2cbd7ebb31bab70c4b4f9b8
MB <mayankbhandari9650@gmail.com>
2025-01-01 13:02:17 +0530

    initial server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,24 @@
-console.log("server");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+require("dotenv").config();
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+const mongoURI = process.env.MONGO_URI || '';
+const PORT = process.env.PORT || '4001';
+app.get("/", (req, res) => {
+  res.send("API is UP");
+});
+
+io.on("connection", () => {
+  console.log("connect");
+});
+
+mongoose.connect(mongoURI).then(() => {
+  console.log("connected to mongodb");
+  httpServer.listen(PORT, () => {
+    console.log(`API is listening on port 4001`);
+  });
+});
